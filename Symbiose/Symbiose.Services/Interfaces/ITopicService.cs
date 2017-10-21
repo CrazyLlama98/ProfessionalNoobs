01097613a01466c4a120b3a43dd51ccb154c1d65
Stefan Iordache <iordachestefan1@gmail.com>
2017-10-21 18:45:26 +0300

    ApplicationServices - Added TopicService

---
+++ Symbiose/Symbiose.Services/Interfaces/ITopicService.cs
@@ -0,0 +1,16 @@
+﻿using Symbiose.Data.Models.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symbiose.Services.Interfaces
+{
+    interface ITopicService : IGenericDbService
+    {
+        IQueryable<Topic> GetTopicsOfProject(int projectId);
+
+        IQueryable<TopicMessage> GetMessagesOfTopic(int topicId);
+    }
+}
