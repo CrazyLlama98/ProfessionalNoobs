c981946a2cfdc114a8397ea4f58c53467d660ea9
Andrei Riclea <liviuriclea@live.com>
2017-10-21 14:31:15 +0300

    accounts models

---
+++ Symbiose/Symbiose.Data/Models/Account/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Symbiose.Data.Models.Account
+{
+    public class User : IdentityUser<int>
+    {
+    }
+}
---
+++ Symbiose/Symbiose.Data/Models/Account/UserRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Symbiose.Data.Models.Account
+{
+    public class UserRole : IdentityRole<int>
+    {
+    }
+}
