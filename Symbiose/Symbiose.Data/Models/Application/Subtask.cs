dfe8ec8390b7be2590f0e137ba0180ac76e3427c
Stefan Iordache <iordachestefan1@gmail.com>
2017-10-21 15:07:39 +0300

    ApplicationData - Added Project, Task, Subtask +Context

---
+++ Symbiose/Symbiose.Data/Models/Application/Subtask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Symbiose.Data.Models.Application
+{
+    class Subtask : Entity
+    {
+        public int DerivedFromId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime Deadline { get; set; }
+
+        public DateTime DateEnded { get; set; }
+
+        public int CreatedById { get; set; }
+
+        public int AssignedToId { get; set; }
+
+        public TaskStatus TaskStatus { get; set; }
+    }
+}
---
+++ Symbiose/Symbiose.Data/SymbioseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Symbiose.Data.Models.Application;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Symbiose.Data
+{
+    class SymbioseContext : DbContext
+    {
+       public SymbioseContext(DbContextOptions<SymbioseContext> dbOptions)
+            : base(dbOptions)
+        {
+            
+        }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        public DbSet<Subtask> Subtasks { get; set; }
+    }
+}
